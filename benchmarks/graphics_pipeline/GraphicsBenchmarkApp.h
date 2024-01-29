6c80a62469a2090e3c0ef78ed410ae50c018ae74
wangra <wangra@google.com>
2024-01-28 20:26:55 -0500

    add support for multiple yuv textures

--- benchmarks/graphics_pipeline/GraphicsBenchmarkApp.h
+++ benchmarks/graphics_pipeline/GraphicsBenchmarkApp.h
@@ -42,6 +42,8 @@ static constexpr const char* kShaderBaseDir   = "benchmarks/shaders";
 static constexpr const char* kQuadTextureFile = "benchmarks/textures/resolution.jpg";
 static constexpr const char* kYUVTextureFile  = "benchmarks/textures/yuv.raw";
 
+static constexpr uint32_t kYuvImageCount = 4;
+
 enum class DebugView
 {
     DISABLED = 0,
@@ -441,7 +443,7 @@ private:
     RealtimeValue<uint64_t, float>    mGpuWorkDuration;
     grfx::SamplerPtr                  mLinearSampler;
     grfx::SamplerPtr                  mPointSampler;
-    grfx::SamplerPtr                  mYuvSampler;
+    grfx::SamplerPtr                  mYuvSampler[kYuvImageCount];
     grfx::DescriptorPoolPtr           mDescriptorPool;
     std::vector<OffscreenFrame>       mOffscreenFrame;
     RealtimeValue<double>             mCPUSubmissionTime;
@@ -478,7 +480,7 @@ private:
     grfx::TexturePtr                                                     mQuadsTexture2;
     grfx::TexturePtr                                                     mQuadsTexture3;
     grfx::TexturePtr                                                     mQuadsTexture4;
-    grfx::TexturePtr                                                     mYUVTexture0;
+    grfx::TexturePtr                                                     mYUVTexture[kYuvImageCount];
     grfx::BufferPtr                                                      mQuadsDummyBuffer;
     QuadPipelineMap                                                      mQuadsPipelines;
     std::array<grfx::PipelineInterfacePtr, kFullscreenQuadsTypes.size()> mQuadsPipelineInterfaces;
